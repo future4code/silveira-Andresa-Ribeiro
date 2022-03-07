005f9ba81996d95743d85fe31e3e7ddf2507c915
Andresa Alves <andresa_15ga@hotmail.com>
2022-03-07 11:54:25 -0300

    Atividade 06 concluida

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -45,7 +45,15 @@ function retornaNumerosParesElevadosADois(array) {
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+   var maior = array[0];
+    
+    for (let i = 1; i < array.length; ++i){
+
+        if (array[i] > maior){ 
+            maior = array[i];
+        }
+    }
+    return maior;
 }
 
 // EXERCÍCIO 07
