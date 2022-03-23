fe2dd99f0693f7920efd1a1b96cc36f9cf1544c3
Andresa Alves <andresa_15ga@hotmail.com>
2022-03-23 16:29:03 -0300

    Atividade Componentes de Classe + Estados

---
+++ modulo2/estados-insta4/insta4/insta4/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'miguel'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+
+      <Post
+          nomeUsuario={'andresa'}
+          fotoUsuario={'https://picsum.photos/200/300'}
+          fotoPost={'https://picsum.photos/seed/picsum/200/150'}
+        />
+
+      <Post
+          nomeUsuario={'paulo'}
+          fotoUsuario={'https://picsum.photos/300/700'}
+          fotoPost={'https://picsum.photos/id/1020/4288/2848'}
+        />
+
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
