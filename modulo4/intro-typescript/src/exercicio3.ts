bfdd650513dd294e0929137f4c8a31b728b17db7
Andresa Alves <andresa_15ga@hotmail.com>
2022-05-25 15:26:25 -0300

    Atividade intro a ts

---
+++ modulo4/intro-typescript/src/exercicio3.ts
@@ -0,0 +1,9 @@
+function checaAnoBissexto(ano: any): boolean {
+    const cond1 = ano % 400 === 0
+    const cond2 = (ano % 4 === 0) && (ano % 100 !== 0)
+    return cond1 || cond2
+ }
+
+ console.log(checaAnoBissexto(2022))
+
+ 
